6a516e93377a7bc722eb25111ae7aa98a61d6f39
Jake Stevenson <jakes@jakes.us>
2013-07-28 20:03:56 -0500

    continue fetching forecast

--- Server/lib/weather/app.js
+++ Server/lib/weather/app.js
@@ -6,12 +6,16 @@ var options = {
 var weather = exports;
 forecast = new Forecast(options);
 
+var getForecast = function(){
+	console.log('fetching');
 	forecast.get(29.7750, -95.6130,function (err, res, data) {
 		var summary = (data["minutely"]["summary"]);
 		var icon = (data["icon"]);
 		var precipProbability = data["currently"]["precipProbability"];
 		var precipIntensity = data["currently"]["precipIntensity"];
+		console.log("Latest forecast: " + summary);
 	});
+};
 
-
-
+getForecast();
+setInterval(getForecast, 3600000);
