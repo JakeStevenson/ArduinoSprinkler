1140204f4cdf2844fc2716ee3b2f1167527a3d07
Jake Stevenson <jakes@jakes.us>
2013-09-16 10:23:41 -0500

    Fix width of progress bar

--- Server/lib/progress.js
+++ Server/lib/progress.js
@@ -4,7 +4,8 @@ var timer;
 
 exports.runBar = function(timeToRun, zone){
 	exports.cancelBar();
-	var bar = new ProgressBar('[:bar] :percent', {total: timeToRun/100});
+	debugger
+	var bar = new ProgressBar('[:bar] :percent', {total: timeToRun/100, width: 60});
 	console.log("Running zone " + zone);
 	timer = setInterval(function(){
 		bar.tick();
