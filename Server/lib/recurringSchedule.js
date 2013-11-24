a3a3fdc953aa9305e68a6931920c94eeebffc68b
Jake Stevenson <jakestevenson@gmail.com>
2013-11-24 11:09:44 -0600

    Load up multiple cron jobs

--- Server/lib/recurringSchedule.js
+++ Server/lib/recurringSchedule.js
@@ -7,26 +7,29 @@ var app = require("../app.js");
 
 var recurringSchedule = exports;
 var todayCancelled = false;
+var jobs = [];
 
 //Startup
 //If a daily job is defined in the config, set it up
 var localStorage = new storage(path.join(process.cwd(), "scratch"));
-var schedule = JSON.parse(localStorage.getItem('schedule'));
-console.log(schedule);
-if(schedule){
-	var dailyJob = scheduler.scheduleJob(schedule[0].cron, function(){
-		//Pass in the config array to runZoneTimes
-		console.log("Beginning scheduled run");
-		schedulemaster.runZoneTimes.apply(undefined, schedule[0].zones);
-		app.io.sockets.emit('nextScheduled', recurringSchedule.nextScheduled())
+var storedSchedule = JSON.parse(localStorage.getItem('schedule'));
+console.log(storedSchedule);
+if(storedSchedule){
+	storedSchedule.forEach(function(schedule)
+	{
+		jobs.push(scheduler.scheduleJob(schedule.cron, function(){
+			//Pass in the config array to runZoneTimes
+			console.log("Beginning scheduled run");
+			schedulemaster.runZoneTimes.apply(undefined, schedule.zones);
+			app.io.sockets.emit('nextScheduled', recurringSchedule.nextScheduled())
+		}));
 	});
-	debugger;
 }
 
 //Exports
 recurringSchedule.nextScheduled = function(){
-	if(dailyJob){
-		return dailyJob.nextInvocation();
+	if(jobs.length>0){
+		return jobs[0].nextInvocation();
 	}
 	return "";
 };
--- Server/setupStorage.js
+++ Server/setupStorage.js
@@ -2,9 +2,14 @@ var storage = require('node-localstorage').LocalStorage;
 
 localStorage = new storage('./scratch');
 localStorage.setItem('schedule', JSON.stringify(
-	[{
-		cron: "42 * * * *",
-		zones: [.2,.1,.1,.1]
+	[
+		{
+			cron: "40 9 * * *",
+			zones: [.5,.1,.1,.1]
+		},
+		{
+			cron: "0 7 * * 1-5",
+			zones: [20,10,10,10]
 		}
 	]
 	));
